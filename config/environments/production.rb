95b9a5a2ded6a1a6d6a4df0d6159c55a9c10bfc6
Mark Mansour <markmans@amazon.com>
2015-09-21 01:35:51 +0000

    Use CloudFront instead of S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,9 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-#  config.action_controller.asset_host = "//s3.amazonaws.com/#{ENV['S3_ASSET_BUCKET']}"
-  config.action_controller.asset_host = "//s3.amazonaws.com/markmans-reinvent-demo-assets"
+  # config.action_controller.asset_host = "//s3.amazonaws.com/#{ENV['S3_ASSET_BUCKET']}"  # may not work
+  # config.action_controller.asset_host = "//s3.amazonaws.com/markmans-reinvent-demo-assets"
+  config.action_controller.asset_host = "//dx9fygs5jbfao.cloudfront.net"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
